a8cfcbe650d3b4dad77a0bb1e62665009f7c5b9a
uladzimir <vova108vova@gmail.com>
2023-05-02 12:48:32 +0300

    componet, function, style rules and others

---
+++ my-app/src/MyApp.js
@@ -0,0 +1,45 @@
+import react from './react.jpg';
+import './App.css';
+
+function Image () {
+  return <img src={ react } alt="react wallpaper" />
+}
+const Header = () => {
+  const styles = {
+    color: '#2F1755',
+    padding: '0 auto',
+    textAlign: 'center'
+  }
+  return (<h1 style={ styles }>Hello React!</h1>)
+}
+function Button() {
+  const textBtn = () => {
+    return 'Log In'
+  }
+  const logged = true
+  const styles = {
+    display: 'block',
+    width: '50%',
+    fontSize: '24px',
+    backgroundColor: '#fff',
+    color: '#2F1755',
+    margin: '0 auto',
+    textAlign: 'center',
+    border: '2px solid #2F1755',
+  }
+  return <button
+            style={styles}>
+            {logged ? 'Enter' : textBtn()}
+        </button>
+}
+function MyApp() {
+  return (
+    <div className="MyApp">
+      <Header />
+      <Image />
+      <Button />
+    </div>
+  );
+}
+
+export default MyApp;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,40 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App'
+// import App from './App'
+import MyApp from './MyApp'
 // import reportWebVitals from './reportWebVitals';
-const text = 'lorem ipsum dolor sit amet, consectetur adip'
-const labelForInput = 'input'
-const component = (
-  <div>
-    <h1 className="first">Hello World!</h1>
-    <div>Text: {text}</div>
-    <div>2 + 2 = { 2 + 2 }</div>
-    <div>2 + 2 = {[4]}</div>
-    <div>2 + 2 = {[2, 2]}</div>
-    <div>2 + 2 = { +[ 2 ] + +[ 2 ] }</div>
-    {/* <div>2 + 2 = { {4} }</div> WILL BE  ERROR*/}
-    {/* <div>2 + 2 = { new Date() }</div> WILL BE  ERROR*/}
-    <label htmlFor={labelForInput}>Write React </label>
-    <input id='input' type="text" tabIndex="0" />
-    <fieldset>
-      <legend>Details</legend>
-      <details>
-        <summary>Summary</summary>
-        <div className="divDetails">Lorem ipsum dolor sit amet consectetur adipisicing elit. </div>
-        <div className="divDetails"> Nostrum magni repellendus officia aliquid, ab itaque illum possimus esse atque quis</div>
-        <div className="divDetails">eum consectetur porro dolores quidem rerum, ad minima </div>
-      </details>
-    </fieldset>
-    <button>BUTTON</button>
-    <button />
-  </div>
-)
+//const text = 'lorem ipsum dolor sit amet, consectetur adip'
+//const labelForInput = 'input'
+//const component = (
+//  <div>
+//    <h1 className="first">Hello World!</h1>
+//    <div>Text: {text}</div>
+//    <div>2 + 2 = { 2 + 2 }</div>
+//    <div>2 + 2 = {[4]}</div>
+//    <div>2 + 2 = {[2, 2]}</div>
+//    <div>2 + 2 = { +[ 2 ] + +[ 2 ] }</div>
+//    {/* <div>2 + 2 = { {4} }</div> WILL BE  ERROR*/}
+//    {/* <div>2 + 2 = { new Date() }</div> WILL BE  ERROR*/}
+//   <label htmlFor={labelForInput}>Write React </label>
+//    <input id='input' type="text" tabIndex="0" />
+//    <fieldset>
+//      <legend>Details</legend>
+//      <details>
+//        <summary>Summary</summary>
+//        <div className="divDetails">Lorem ipsum dolor sit amet consectetur adipisicing elit. </div>
+//        <div className="divDetails"> Nostrum magni repellendus officia aliquid, ab itaque illum possimus esse atque quis</div>
+//        <div className="divDetails">eum consectetur porro dolores quidem rerum, ad minima </div>
+//    </details>
+//    </fieldset>
+//    <button>BUTTON</button>
+//    <button />
+//  </div>
+//)
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  component,
   <React.StrictMode>
-    <App />
+    <MyApp />
   </React.StrictMode>
 )
 
